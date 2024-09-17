8a07ceca6d0620726dcbc967c4b3731d63d11c44
Chbabula <mai2babulal786@gmail.com>
2024-09-17 15:38:00 +0530

    CounterApp

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,15 @@ const addVal = ()=>{
 const remVal=()=>{
    return setCounter(count-1)
 }
-
   return (
     <div className="mainConatiner">
+      <div className="container">
+
       <h1 >This is counter app!</h1>
       <h2>This is the counter: <span>{count}</span></h2>
       <button onClick={addVal}>Add Value</button>{" "}
       <button onClick={remVal}>Remove Value</button>
+      </div>
     </div>
   )
 }
